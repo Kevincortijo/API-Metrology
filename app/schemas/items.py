d0c74b869d9de994eea02b189c57ba7ef8faab12
Joel Junior <joelrool.junior@lgepartner.com>
2025-07-31 14:39:24 -0400

    :sparkles: feat: add items Schemas to database

--- app/schemas/items.py
+++ app/schemas/items.py
@@ -1,10 +1,28 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
-class ItemCreate(BaseModel):
+class ItemBase(BaseModel):
     name:str
     code:str
     quantity:int
     supplier:str
     unit_price:float
-    descripition:str
+    descripition:str    
+
+class ItemCreate(ItemBase):pass
+
+class ItemRead(ItemBase):
+    id:int
+
+    class Config:
+        orm_mode=True
+
+class ItemUpdate(ItemBase):
+    name:Optional[str] = None
+    code:Optional[str] = None
+    quantity:Optional[int] = None
+    supplier:Optional[str] = None
+    unit_price:Optional[float] = None
+    descripition:Optional[str] = None    
