7e87929c57cea15d0a6e750257ce1de67bc1f2d6
Joel Junior <joelrool.junior@lgepartner.com>
2025-08-04 14:43:45 -0400

    :sparkles: feat: first steps to integration with angular project

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.user_routes import router as user_router
 from app.api.item_routes import router as item_router
 from app.db.database_config import Base, engine
 
 app = FastAPI(title='API Metrologia')
+app.add_middleware(CORSMiddleware,
+                   allow_origins=['*'],
+                   allow_credentials=True,
+                   allow_methods=['*'],
+                   allow_headers=['*'],
+                   )
 Base.metadata.create_all(engine)
 
 app.include_router(user_router)
